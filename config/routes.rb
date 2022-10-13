b925511028745e31f4852140eb426563b5e9fe95
Mayank Sinha <mayank.sinha@oaktreecloud.com>
2022-10-14 00:32:31 +0530

    Implement Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'inquiries/index'
+  
   devise_for :users
   root to: "cars#index"
 
+  get 'inquiries/index'
   get 'home/index'
   get '/my_cars', to: 'cars#user_cars'
   get '/brand/:brand', to: 'cars#index'
